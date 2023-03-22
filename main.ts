6f8eebee186c2bfd9c6cb690968fe9a25f0d2646
tijn-ui <128613646+tijn-ui@users.noreply.github.com>
2023-03-22 15:30:09 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motorStop(maqueen.Motors.M1)
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+                maqueen.motorStop(maqueen.Motors.M1)
+            }
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                maqueen.motorStop(maqueen.Motors.M2)
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                    maqueen.motorStop(maqueen.Motors.M2)
+                }
+                if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+                } else {
+                    maqueen.motorStop(maqueen.Motors.M2)
+                }
+            }
+        }
+    }
+})
